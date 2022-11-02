5a5b862060464d4d807d5af8a06f8d8ed59aeb13
surveymethodsroombudapest <116962770+surveymethodsroombudapest@users.noreply.github.com>
2022-11-02 14:57:50 +0100

    multilang

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,12 @@
+const { EleventyI18nPlugin } = require("@11ty/eleventy");
+
 module.exports = function (config){
+    
+
+    eleventyConfig.addPlugin(EleventyI18nPlugin, {
+        // any valid BCP 47-compatible language tag is supported
+        defaultLanguage: "", // Required, this site uses "en"
+      });
     
     config.addPassthroughCopy({"./src/static": ""})
 
