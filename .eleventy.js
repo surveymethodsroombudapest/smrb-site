4b1a5e16a7404c93c865e86bce5d73db6a5d6092
Survey Methods <surveymethods.site@gmail.com>
2022-12-31 11:43:59 +0100

    moving to pnpm

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,14 @@
-// const minify = require('html-minifier')
 const fs = require("fs");
 const NOT_FOUND_PATH = "public/404.html";
+const i18n = require("eleventy-plugin-i18n");
 const moment = require("moment");
-const i18n = require('eleventy-plugin-i18n');
-const translations = require('./src/_data/translations.js')
-const mainnav = require('./src/_data/mainnav.js')
+const translations = require("./src/_data/translations.js");
+const mainnav = require("./src/_data/mainnav.js");
 
 module.exports = function (eleventyConfig) {
-
   // Passthroughs
-  eleventyConfig.addPassthroughCopy({ "./src/static/": "/" })
-  eleventyConfig.addPassthroughCopy("./src/css/")
+  eleventyConfig.addPassthroughCopy({ "./src/static/": "/" });
+  eleventyConfig.addPassthroughCopy("./src/css/");
 
   // Layout aliases
   eleventyConfig.addLayoutAlias("base", "layouts/base.njk");
@@ -26,29 +24,33 @@ module.exports = function (eleventyConfig) {
     return collection.getFilteredByGlob("./src/en/news/!(index).md").reverse();
   });
   eleventyConfig.addCollection("kutatok_hu", (collection) => {
-    return collection.getFilteredByGlob("./src/hu/kutatok/!(index).md")
+    return collection.getFilteredByGlob("./src/hu/kutatok/!(index).md");
   });
   eleventyConfig.addCollection("kutatok_en", (collection) => {
-    return collection.getFilteredByGlob("./src/en/researchers/!(index).md")
+    return collection.getFilteredByGlob("./src/en/researchers/!(index).md");
   });
   eleventyConfig.addCollection("proj_hu", (collection) => {
-    return collection.getFilteredByGlob("./src/hu/projektek/!(index).md").reverse();
+    return collection
+      .getFilteredByGlob("./src/hu/projektek/!(index).md")
+      .reverse();
   });
   eleventyConfig.addCollection("proj_en", (collection) => {
-    return collection.getFilteredByGlob("./src/en/projects/!(index).md").reverse();
+    return collection
+      .getFilteredByGlob("./src/en/projects/!(index).md")
+      .reverse();
   });
   eleventyConfig.addCollection("publ_hu", (collection) => {
-    return collection.getFilteredByGlob("./src/hu/publikaciok/!(index).md")
+    return collection.getFilteredByGlob("./src/hu/publikaciok/!(index).md");
   });
   eleventyConfig.addCollection("publ_en", (collection) => {
-    return collection.getFilteredByGlob("./src/en/publications/!(index).md")
+    return collection.getFilteredByGlob("./src/en/publications/!(index).md");
   });
 
   eleventyConfig.addPlugin(i18n, {
     translations,
     fallbackLocales: {
-      'hu': 'en'
-    }
+      hu: "en",
+    },
   });
 
   // date filter (localized)
@@ -59,46 +61,30 @@ module.exports = function (eleventyConfig) {
   });
 
   eleventyConfig.addFilter("localisedLink", (id, locale) => {
-    return mainnav.find(item => item.id === id)[locale].link
-  })
+    return mainnav.find((item) => item.id === id)[locale].link;
+  });
   eleventyConfig.addFilter("localisedLinkLabel", (id, locale) => {
-    return mainnav.find(item => item.id === id)[locale].label
-  })
+    return mainnav.find((item) => item.id === id)[locale].label;
+  });
   eleventyConfig.addShortcode("localisedLink", (id, locale) => {
-    return mainnav.find(item => item.id === id)[locale].link
-  })
+    return mainnav.find((item) => item.id === id)[locale].link;
+  });
 
-  eleventyConfig.addNunjucksShortcode("goback", (link, text) => `
+  eleventyConfig.addNunjucksShortcode(
+    "goback",
+    (link, text) => `
     <a href="${link}" class="goback"><i class="bi bi-caret-left"></i> ${text}</a>
-  `);
-  /*
-  // Minify HTML
-const isProduction = process.env.ELEVENTY_ENV === "production";
-
-var htmlMinify = function(value, outputPath) {
-  if (outputPath && outputPath.indexOf('.html') > -1) {
-    return minify(value, {
-      useShortDoctype: true,
-      removeComments: true,
-      collapseWhitespace: true,
-      minifyCSS: true
-    });
-  }
-}
-
-// html min only in production
-if (isProduction) {
-  eleventyConfig.addTransform("htmlmin", htmlMinify);
-}
-  */
+  `
+  );
 
   eleventyConfig.setBrowserSyncConfig({
     callbacks: {
       ready: function (err, bs) {
-
         bs.addMiddleware("*", (req, res) => {
           if (!fs.existsSync(NOT_FOUND_PATH)) {
-            throw new Error(`Expected a \`${NOT_FOUND_PATH}\` file but could not find one. Did you create a 404.html template?`);
+            throw new Error(
+              `Expected a \`${NOT_FOUND_PATH}\` file but could not find one. Did you create a 404.html template?`
+            );
           }
 
           const content_404 = fs.readFileSync(NOT_FOUND_PATH);
@@ -108,18 +94,18 @@ if (isProduction) {
           res.write(content_404);
           res.end();
         });
-      }
-    }
+      },
+    },
   });
 
   return {
-    markdownTemplateEngine: 'njk',
-    dataTemplateEngine: 'njk',
-    htmlTemplateEngine: 'njk',
+    markdownTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
     dir: {
       input: "src",
       output: "public",
-      data: "_data"
-    }
-  }
-}
+      data: "_data",
+    },
+  };
+};
