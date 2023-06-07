437ebfc2578b76f85155279aacdd31d481e0b398
--global <--global>
2023-06-07 22:46:17 +0200

    masonry layout

---
+++ src/static/masonry.js
@@ -0,0 +1,22 @@
+
+var media = window.matchMedia("(min-width: 768px)")
+
+function listenConditionally () {
+  if (media.matches) {
+    let magicGrid = new MagicGrid({
+        container: ".masonry-grid", // Required. Can be a class, id, or an HTMLElement.
+        static: true, // Required for static content.
+        useMin: true,
+        animate: false,
+        gutter: 20
+      });
+    magicGrid.listen();
+    console.log("match!")
+  } else {
+    console.log("clear!")
+  }
+}
+
+media.addListener(listenConditionally);
+
+listenConditionally();
